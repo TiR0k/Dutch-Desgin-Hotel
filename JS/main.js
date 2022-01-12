015466c4962b9eed13f4da58f62c56d90e08938e
Tim Roks <s1130082@student.hsleiden.nl>
2022-01-12 21:34:21 +0100

    dynamically added 10 markers

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+const scene = document.getElementById("scene");
+const body = document.getElementById("body");
 //https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
 
 fetch("https://type.fit/api/quotes")
@@ -7,7 +9,12 @@ fetch("https://type.fit/api/quotes")
     .then(function(data) {
         console.log(data);
 
+        for(let i=0; i<10; i++) {
+            scene.innerHTML += "<a-marker type='barcode' value= '" + i + "'> <a-text text='"+ data[i].text + "' rotation='-90 0 0'></a-text></a-marker>"
+        }
+
     })
     .catch(err => {
         console.error("Error", err);
     });
+
