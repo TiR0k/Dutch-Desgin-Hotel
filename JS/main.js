bb84b5b9fd382f68f4666207d5d4508b67470f01
Tim Roks <s1130082@student.hsleiden.nl>
2022-01-15 15:59:36 +0100

    dynamically adds quotes, markers now hardcoded, marker code images added

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 const scene = document.getElementById("scene");
 const body = document.getElementById("body");
+const quotes = document.getElementsByClassName("js--quote");
 //https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
 
 fetch("https://type.fit/api/quotes")
@@ -9,8 +10,8 @@ fetch("https://type.fit/api/quotes")
     .then(function(data) {
         console.log(data);
 
-        for(let i=0; i<10; i++) {
-            scene.innerHTML += "<a-marker type='barcode' value= '" + i + "'> <a-text text='"+ data[i].text + "' rotation='-90 0 0'></a-text></a-marker>"
+        for(let i=0; i<quotes.length; i++) {
+            quotes[i].setAttribute("value", data[i].text);
         }
 
     })
