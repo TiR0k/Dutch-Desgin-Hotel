6a7ef987b2abad4fedde02bb791e7e91087785f4
Martin Krikke <martin.krikke10@gmail.com>
2022-01-22 16:29:05 +0100

    verfkwast erin

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,25 @@
-const scene = document.getElementById("scene");
-const body = document.getElementById("body");
-const quotes = document.getElementsByClassName("js--quote");
-//https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
+window.onload = () => {
+    const scene = document.getElementById("scene");
+    const body = document.getElementById("body");
+    const quotes = document.getElementsByClassName("js--quote");
+    //https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
 
-fetch("https://type.fit/api/quotes")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
+    fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
 
-        for(let i=0; i<quotes.length; i++) {
-            quotes[i].setAttribute("value", "\"" + data[i].text + "\" ~" + data[i].author);
-        }
+            for(let i=0; i<quotes.length; i++) {
+                quotes[i].setAttribute("value", "\"" + data[i].text + "\" ~" + data[i].author);
+            }
 
-    })
-    .catch(err => {
-        console.error("Error", err);
-    });
+        })
+        .catch(err => {
+            console.error("Error", err);
+        });
+    
+}
+
 
