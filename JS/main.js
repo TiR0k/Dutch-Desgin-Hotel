95052a50180c2105eac82e3e1a6342f7074153ba
Martin Krikke <martin.krikke10@gmail.com>
2022-01-25 12:21:44 +0100

    feiten functionaliteit

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@ window.onload = () => {
     const scene = document.getElementById("scene");
     const body = document.getElementById("body");
     const quotes = document.getElementsByClassName("js--quote");
+    
     //https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
 
     fetch("https://type.fit/api/quotes")
@@ -19,6 +20,56 @@ window.onload = () => {
         .catch(err => {
             console.error("Error", err);
         });
+    
+    
+    // TEXT / FEITEN FUNCTIONALITEIT
+
+    // show facts
+    const showFacts = (file, text) => {
+        let i = 0;
+        let facts = text.split("\n");
+        setInterval(() => {
+            factsLoop(file, facts, i);
+        }, delay * facts.length);
+    }
+
+    // loop facts
+    let delay = 5000; // time between facts in ms
+    const factsLoop = (file, facts, i) => {
+        setTimeout(() => {
+            switch(file) {
+                case 'files/bigunknown.txt':
+                    bigunknowntext.setAttribute("value", facts[i]);
+                    break;
+                case 'files/anderetext.txt':
+                    anderetext.setAttribute("value", facts[i]);
+                    break;    
+            }
+            i++;
+            if (i < facts.length) {
+                factsLoop(file, facts, i);
+            }
+        }, delay);
+    }
+
+    const bigunknowntext = document.getElementById("js--bigunknowntext");
+    const anderetext = document.getElementById("js--anderetext");
+    function readTextFile(file) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function () {
+            if(rawFile.readyState === 4) {
+                if(rawFile.status === 200 || rawFile.status == 0) {
+                    let allText = rawFile.responseText;
+                    showFacts(file, allText);
+                }
+            }
+        }
+        rawFile.send(null);
+    }
+
+    readTextFile('files/bigunknown.txt');
+    readTextFile('files/anderetext.txt');
     
 }
 
