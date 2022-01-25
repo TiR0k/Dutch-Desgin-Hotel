4a833af9f132d5a8ac012d651a26e3b96a7b20d4
Martin Krikke <martin.krikke10@gmail.com>
2022-01-25 14:23:02 +0100

    functionaliteit chiller gemaakt

--- JS/main.js
+++ JS/main.js
@@ -28,29 +28,42 @@ window.onload = () => {
     const showFacts = (file, text) => {
         let i = 0;
         let facts = text.split("\n");
-        setInterval(() => {
-            factsLoop(file, facts, i);
-        }, delay * facts.length);
+        facts.push(""); // zorgt ervoor dat de loop zonder tekst eindigt
+        factsLoop(file, facts, i);
+    }
+
+    const setFile = (file, facts, i) => {
+        switch(file) {
+            // zet je file en tekst.setAttribute hierbij
+            case 'files/bigunknown.txt':
+                bigunknowntext.setAttribute("value", facts[i]);
+                break;
+            case 'files/anderetext.txt':
+                anderetext.setAttribute("value", facts[i]);
+                break;
+        }
     }
 
     // loop facts
+    let timeout = false;
     let delay = 5000; // time between facts in ms
     const factsLoop = (file, facts, i) => {
-        setTimeout(() => {
-            switch(file) {
-                // zet je file en tekst.setAttribute hierbij
-                case 'files/bigunknown.txt':
-                    bigunknowntext.setAttribute("value", facts[i]);
-                    break;
-                case 'files/anderetext.txt':
-                    anderetext.setAttribute("value", facts[i]);
-                    break;    
-            }
+        if (!timeout) { // zorgt ervoor dat er geen delay is voor het eerste element
+            setFile(file, facts, i);
+        }
+        timeout = true;
+        setTimeout(() => {  
+            setFile(file, facts, i);
             i++;
             if (i < facts.length) {
                 factsLoop(file, facts, i);
             }
+            if (i == facts.length) {
+                factsRunning = false;
+                return;
+            }
         }, delay);
+        
     }
 
     // declareer je tekst element
@@ -71,8 +84,27 @@ window.onload = () => {
         rawFile.send(null);
     }
 
-    // zet je file hierbij
-    readTextFile('files/bigunknown.txt');
-    readTextFile('files/anderetext.txt');
-    
+    var factsRunning = false;
+
+    // declareer je marker
+    const bigunknownmarker = document.getElementById('bigunknown');
+    const anderemarker = document.getElementById('anderemarker');
+
+    AFRAME.registerComponent('markerhandler', {
+        tick: function() {           
+            if(bigunknownmarker.object3D.visible == true) { // voeg else if's toe van je eigen marker
+                if (!factsRunning) {
+                    readTextFile('files/bigunknown.txt');
+                    factsRunning = true;
+                }
+            }
+            else if (anderemarker.object3D.visible == true) {
+                if (!factsRunning) {
+                    readTextFile('files/anderetext.txt');
+                    factsRunning = true;
+                }
+            }
+
+        }
+    });
 }
